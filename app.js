fc539cdd0d1ee125107bb9ed2ad7017b34c7b731
Faye <lis@rose-hulman.edu>
2016-03-03 18:39:26 -0500

    Added sushi background

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ var main = function() {
 	$('.hamburger').click(function(){
 		$('.navmenu').toggleClass('hidden-xs');
 	});
+
+	var controller = new ScrollMagic.Controller();
+	var scene = new ScrollMagic.Scene()
+		.setPin("#sidepin")
+		.addIndicators()
+		.addTo(controller);
 }
 
 $(document).ready(main);

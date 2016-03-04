9c78bf23432360184791a2c6472b2a614a778bf1
Faye <lis@rose-hulman.edu>
2016-03-03 21:24:21 -0500

    Parallax sushi scrolling

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ var main = function() {
 	});
 
 	var controller = new ScrollMagic.Controller();
-	var scene = new ScrollMagic.Scene()
-		.setPin("#sidepin")
+	var tween = new TimelineMax()
+		.add([
+			TweenMax.to("#sushiParallax .sushi1", 1, {y: "-225%", ease: Linear.easeNone}),
+			TweenMax.to("#sushiParallax .sushi2", 1, {y: "-500%", ease: Linear.easeNone})
+			]);
+
+	var scene1 = new ScrollMagic.Scene({triggerElement: "#sushiParallax", duration: 2000})
+		.setTween(tween)
+		.setPin("#sushiParallax")
 		.addIndicators()
 		.addTo(controller);
 }
